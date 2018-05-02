9bf1693d22eb903aa341ca1e4718e631eb525731
rendiix <vanzdobz@gmail.com>
2018-05-03 00:46:35 +0700

    adds a selection of ubuntu version

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,56 +1,77 @@
 #!/data/data/com.termux/files/usr/bin/bash
-cur=$(pwd)
-folder="$cur/ubuntu-fs"
+folder=ubuntu-fs
 if [ -d "$folder" ]; then
-    first=1
-    echo "skipping downloading"
+	first=1
+	echo "skipping downloading"
 fi
+while [[ $env != 0 ]]; do
+u_version="Please select the ubuntu version:
+
+    1. Ubuntu 18.04 Bionic
+    2. Ubuntu 17.10 Artful
+    3. Ubuntu 16.04 Xenial
+    
+"
+echo -e "$u_version"
+read env;
+case $env in
+  1) echo -e "\nInstalling Ubuntu 18.04 Bionic"
+      ubuntu_version="bionic";;
+  2) echo -e "\nInstalling Ubuntu 17.10 Artful"
+      ubuntu_version="arful";;
+  3) echo -e "\nInstalling Ubuntu 16.04 Xenial"
+      ubuntu_version="xenial";;
+  *) echo -e "\Please enter the correct option";;
+esac
+tarball="ubuntu.tar.gz"
 if [ "$first" != 1 ];then
-    if [ ! -f "ubuntu.tar.gz" ]; then
-        echo "downloading ubuntu-image"
-        if [ "$(dpkg --print-architecture)" = "aarch64" ];then
-            wget https://partner-images.canonical.com/core/bionic/current/ubuntu-bionic-core-cloudimg-arm64-root.tar.gz -O ubuntu.tar.gz
-        elif [ "$(dpkg --print-architecture)" = "arm" ];then
-            wget https://partner-images.canonical.com/core/bionic/current/ubuntu-bionic-core-cloudimg-armhf-root.tar.gz -O ubuntu.tar.gz
-        elif [ "$(dpkg --print-architecture)" = "i686" ];then
-            wget https://partner-images.canonical.com/core/bionic/current/ubuntu-bionic-core-cloudimg-i386-root.tar.gz -O ubuntu.tar.gz
-        elif [ "$(dpkg --print-architecture)" = "i386" ];then
-            wget https://partner-images.canonical.com/core/bionic/current/ubuntu-bionic-core-cloudimg-i386-root.tar.gz -O ubuntu.tar.gz
-        elif [ "$(dpkg --print-architecture)" = "amd64" ];then
-            wget https://partner-images.canonical.com/core/bionic/current/ubuntu-bionic-core-cloudimg-amd64-root.tar.gz -O ubuntu.tar.gz
-        else
-            echo "unknown architecture"
-            exit 1
-        fi
-    fi
-    mkdir -p $folder
-    cd $folder
-    echo "decompressing ubuntu image"
-    proot --link2symlink tar -xf $cur/ubuntu.tar.gz --exclude='dev'||:
-    echo "fixing nameserver, otherwise it can't connect to the internet"
-    echo "nameserver 8.8.8.8" > etc/resolv.conf
-    cd $cur
+	if [ ! -f $tarball ]; then
+		echo "downloading ubuntu-image"
+		case `dpkg --print-architecture` in
+		aarch64)
+			archurl="arm64" ;;
+		arm)
+			archurl="armhf" ;;
+		amd64)
+			archurl="amd64" ;;
+		i*86)
+			archurl="i386" ;;
+		*)
+			echo "unknown architecture"; exit 1 ;;
+		esac
+		wget "https://partner-images.canonical.com/core/${ubuntuv_ersion}/current/ubuntu-${ubuntu_version}-core-cloudimg-${archurl}-root.tar.gz" -O $tarball
+	fi
+	cur=`pwd`
+	mkdir -p "$folder"
+	cd "$folder"
+	echo "decompressing ubuntu image"
+	proot --link2symlink tar -xf ${cur}/${tarball} --exclude='dev'||:
+	echo "fixing nameserver, otherwise it can't connect to the internet"
+	echo "nameserver 1.1.1.1" > etc/resolv.conf
+	cd "$cur"
 fi
 mkdir -p binds
-bin=/data/data/com.termux/files/usr/bin/startubuntu
+bin=$PREFIX/bin/ubuntu
 echo "writing launch script"
 cat > $bin <<- EOM
-#!/data/data/com.termux/files/usr/bin/bash
+#!/bin/bash
 cd \$(dirname \$0)
+## unset LD_PRELOAD in case termux-exec is installed
 unset LD_PRELOAD
 command="proot"
 command+=" --link2symlink"
 command+=" -0"
 command+=" -r $folder"
-if [ -n "\$(ls -A $cur/binds)" ]; then
+if [ -n "\$(ls -A binds)" ]; then
     for f in $cur/binds/* ;do
-        . \$f
+      . \$f
     done
 fi
 command+=" -b /system"
-command+=" -b /dev/"
-command+=" -b /sys/"
-command+=" -b /proc/"
+command+=" -b /dev"
+command+=" -b /sys"
+command+=" -b /proc"
+## uncomment the following line to have access to the home directory of termux
 command+=" -b /data/data/com.termux/files/home"
 command+=" -b /sdcard"
 command+=" -w /root"
@@ -67,6 +88,9 @@ else
     \$command -c "\$com"
 fi
 EOM
+chmod 777 $bin
+echo "fixing shebang of $bin"
+termux-fix-shebang $bin
 echo "making $bin executable"
 chmod +x $bin
-echo "You can now launch Ubuntu with " startubuntu ""
+echo "You can now launch Ubuntu with the command ubuntu"
